e56e9093bd4878ce02b093d6111948e459eaf658
Matheus Diniz <matheus.gruber123@gmail.com>
2026-04-27 19:40:06 -0300

    Initial Commit

---
+++ monClipsTests/monClipsTests.swift
@@ -0,0 +1,18 @@
+//
+//  monClipsTests.swift
+//  monClipsTests
+//
+//  Created by Matheus Diniz  on 27/04/26.
+//
+
+import Testing
+
+struct monClipsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
